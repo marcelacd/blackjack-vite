d82fe95923806e0099853842ab8bbfc187633db7
Marcela Carmona <gloriamarcela.carmonaduran@emeal.nttdata.com>
2023-07-05 20:33:32 -0500

    Firsh commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,8 @@
+
+// import crearDeck, {miNombre} from './usecases/crear-deck';
+// import { crearDeck as nuevodeck} from './usecases/crear-deck';
+export { crearCartaHTML } from './crear-carta-html';
+export { crearDeck } from './crear-deck';
+export { pedirCarta } from './pedir-carta';
+export { turnoComputadora } from './turno-computadora';
+export { valorCarta } from './valor-carta';
